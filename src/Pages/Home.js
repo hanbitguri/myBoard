851ca41313fc8c36de4d54c6a56c33af240fb978
전한빛 <asjask123@gmail.com>
2023-02-24 15:59:02 +0900

    🎉 Begin a Project

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; // 그리드 만 빼옴
+import LoginForm from "../Components/LoginForm";
+
+function Home() {
+  return (
+    <>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-4">hi</div>
+      </div>
+    </div>
+    
+    </>
+  );
+}
+
+export default Home;
