090c527a22fa018e55cf734a9ca996ae1ab9857f
전한빛 <asjask123@gmail.com>
2023-02-27 15:21:19 +0900

    🔨 add viewPoint update logic

--- src/Components/BoardItem.js
+++ src/Components/BoardItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function BoardItem({boardItem}) {
   return (
@@ -6,20 +7,28 @@ function BoardItem({boardItem}) {
         {
         boardItem.map((item,index)=>{
             return (
-                <div className='board-item' key={item.id}>
+                <Link to={`/board/${index}`} className='board-item' key={item.id} onClick={()=>{
+                  fetch(`https://react-http-9dfc2-default-rtdb.firebaseio.com/post/${item.id}.json`,{
+                    method:"PATCH",
+                    body:JSON.stringify({
+                      viewPoint:item.viewPoint + 1,
+                    })
+                  })
+
+                }}>
                 <ul className='board-item-list'>
                     <div className='board-item-main'>
                     {/* <li>{index}</li> */}
-                    <li className='board-item-theme'>[자유]</li>
+                    <li className='board-item-theme'>[{item.theme}]</li>
                     <li >{item.title}</li>
                     </div>
                     <div className='board-item-desc'>
                     <li>조회수: {item.viewPoint}</li>
-                    <li >게시일: {item.date}</li>
-                    <li >글쓴이: {item.writer}</li>
+                    <li>게시일: {item.date}</li>
+                    <li>글쓴이: {item.writer}</li>
                     </div>
                 </ul>
-                </div>
+                </Link>
             )
         
     
