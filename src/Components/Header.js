24811dc38ea3b6442a6dcd34ea586abf1bf01768
전한빛 <asjask123@gmail.com>
2023-02-27 22:23:29 +0900

    ✨ add <Link> to Header.js component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Header(props) {
+function Header() {
   return (
     <header className='global-header'>
-        <button className='sidebar'>사이드바</button>
-        <h2 className='main-logo'>메인로고</h2>
-        <button className='user-info'>유저정보</button>
+        <h2 className='header-logo'>★</h2>
+        <nav className='header-nav'>
+        <button><Link to={'/'}>홈</Link></button>
+        <button><Link to={'/board'}>게시판</Link></button>
+        </nav>
+
+        <button className='header-info'><Link to={'/login'}>로그인</Link></button>
     </header>
   )
 }
