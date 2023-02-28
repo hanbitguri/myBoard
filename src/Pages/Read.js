e7ac63d4822c7001c5fb0f08e26e78c41c86f9da
전한빛 <asjask123@gmail.com>
2023-02-28 23:20:54 +0900

    ✨ add read - form password remove logic

--- src/Pages/Read.js
+++ src/Pages/Read.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 
 function Read({boardItem}) {
   const param = useParams()
-  
+  const [readPassword,setReadPassword] = useState('')
   return (
     <section className='read-form'>
       <div className='read-form-header'>
@@ -15,6 +15,26 @@ function Read({boardItem}) {
       </div>
       
       <p className='read-form-main'>{boardItem[param.id].detail}</p>
+
+      <div className='read-form-button'>
+        <input type="password" id='read-form-auth' value={readPassword} placeholder='비밀번호' onChange={(e)=>{
+          setReadPassword(e.target.value)
+        }}/>
+        <button className='read-form-modify'>수정</button>
+        <button className='read-form-remove'  onClick={()=>{
+            if(boardItem[param.id].password === readPassword){
+              fetch(`https://react-http-9dfc2-default-rtdb.firebaseio.com/post/${boardItem[param.id].id}.json`,{
+                method:"DELETE",
+              })
+              
+            }else{
+              alert('비번틀렸어연')
+              
+              
+            }
+        }}>삭제</button>
+        <button className='read-form-board'><Link to={'/board'}>글목록</Link></button>
+      </div>
     </section>
 
         
