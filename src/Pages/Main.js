21e4762c38eb0340f38ab866d3d18ec25496e310
전한빛 <asjask123@gmail.com>
2023-02-28 23:26:22 +0900

    📝 할거 까먹을까봐 써놓음

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div>
+      <p>
+        할거목록<br/>
+        글 등록시 아래에서 위로 렌더,<br/>
+        글 등록시 화면 글목록으로 이동,<br/>
+        글 수정 삭제시 글목록으로 이동,<br/>
+        useEffect 의존성배열 다듬기<br/>
+        +의도치않은 새로고침시 fetch 재호출하기
+        
+      </p>
+    </div>
   )
 }
 
