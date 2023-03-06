605d6350f4feaf33c1c449e47c7287959c518d7b
전한빛 <asjask123@gmail.com>
2023-03-06 22:40:56 +0900

    💄 main-page markup and add style

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,28 +1,44 @@
 import React from 'react'
+import { DiGithubBadge } from "react-icons/di";
+import { CiInstagram } from "react-icons/ci";
+import { AiFillHtml5,} from "react-icons/ai";
+import { FaReact} from "react-icons/fa";
+import { DiJavascript} from "react-icons/di";
+
+
 
 function Main() {
   return (
     <main className='main-page'>
       <p className='main-intro'>
-        안녕하세요! 저는 프론트엔드 엔지니어를 꿈꾸는 <strong>전한빛</strong> 입니다. <br/>
+        안녕하세요! <br/>저는 프론트엔드 엔지니어를 꿈꾸는 <strong>전한빛</strong> 입니다. <br/>
       이 블로그는 제 성장과정을 공유하는 공간입니다.  <br/>
-      제가 어떤 기술과 프로젝트를 수행했는지를 확인하실 수 있습니다. <br/>
+      이곳에서 제가 어떤 기술과 프로젝트를 수행했는지를 확인하실 수 있습니다. <br/>
       </p>
-      <div className='main-tech-stack'></div>
+      <div className='main-tech-stack'>
+
       <h2>Tech Stack</h2>
       <ul>
-        <li>HTML/CSS</li>
-        <li>JavaScript</li>
-        <li>React</li>
+        <li><AiFillHtml5/>HTML/CSS/SCSS</li>
+        <li><DiJavascript/>JavaScript</li>
+        <li><FaReact/>React</li>
       </ul>
+      </div>
 
       <div className='main-project-category'>
         <h2>프로젝트 카테고리</h2>
         <ul>
-          <li>개인 블로그 (myLog)</li>
-          <li>오늘의 집 클론 (내일의 집)</li>
+          <li><a href="http://hanbitguri.github.io/tomorrow-house"> 오늘의 집 클론코딩(내일의 집)</a></li>
+          <li><a href="http://hanbitguri.github.io/mylog">개인 블로그(myLog)</a></li>
         </ul>
-        <span>위 카테고리를 클릭하시면 해당하는 포트폴리오 목록을 확인하실 수 있습니다.</span>
+      </div>
+
+      <div className='main-social'>
+        <h2>소셜 미디어</h2>
+        <ul>
+          <li><DiGithubBadge className='main-social-git'/><a href="http://github.com/hanbitguri">github.com/hanbitguri</a></li>
+          <li><CiInstagram/><a href="https://www.instagram.com/">instagram.com/hanbitguri</a></li>
+        </ul>
       </div>
       
 
