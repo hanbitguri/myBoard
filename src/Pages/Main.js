17e4918b57ef21ddd7e51b7fb8c23b00aa9ba5bd
전한빛 <asjask123@gmail.com>
2023-03-06 22:02:41 +0900

    🚧 main-page update

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,19 +2,33 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>
-      <p>
-        할거목록<br/>
-        
-        useEffect 의존성배열 다듬기<br/>
-        특정구간 새로고침시 fetch 재호출하기<br/>
-        게시판 페이징 만들기<br/>
-        로그인 페이지 없애기<br/>
-        CSS 작업하기<br/>
-        <br/>
-        
+    <main className='main-page'>
+      <p className='main-intro'>
+        안녕하세요! 저는 프론트엔드 엔지니어를 꿈꾸는 <strong>전한빛</strong> 입니다. <br/>
+      이 블로그는 제 성장과정을 공유하는 공간입니다.  <br/>
+      제가 어떤 기술과 프로젝트를 수행했는지를 확인하실 수 있습니다. <br/>
       </p>
-    </div>
+      <div className='main-tech-stack'></div>
+      <h2>Tech Stack</h2>
+      <ul>
+        <li>HTML/CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+
+      <div className='main-project-category'>
+        <h2>프로젝트 카테고리</h2>
+        <ul>
+          <li>개인 블로그 (myLog)</li>
+          <li>오늘의 집 클론 (내일의 집)</li>
+        </ul>
+        <span>위 카테고리를 클릭하시면 해당하는 포트폴리오 목록을 확인하실 수 있습니다.</span>
+      </div>
+      
+
+      
+
+    </main>
   )
 }
 
